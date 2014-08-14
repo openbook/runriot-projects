01cbffef9efd75f8b7d08dc275d2f1fe8b986fd6
Andy Brace <contact@openbook.uk.com>
2014-08-15 00:54:59 +0100

    date fix

--- public/sites/all/themes/custom/runriot_zen/templates/views/views-view-fields--latest-projects--panel-pane-1.tpl.php
+++ public/sites/all/themes/custom/runriot_zen/templates/views/views-view-fields--latest-projects--panel-pane-1.tpl.php
@@ -8,8 +8,10 @@
 </div>
 <div class="pane-desc">
   <h3><?php print $row->_field_data['nid']['entity']->title; ?></h3>
-  <p class="meta-date"><?php print $row->_field_data['nid']['entity']->field_date_time_text['und'][0]['value']; ?></p>
+  <p class="meta-date"><?php print $row->_field_data['nid']['entity']->field_date_time_text['und'][0]['value']; ?>
+  <br /><?php print $row->field_field_time[0]['rendered']; ?></p>
   <p><?php print $row->_field_data['nid']['entity']->field_summary['und'][0]['value']; ?></p>
   <p><a class="btn" href="<?php print url("node/". $row->nid); ?>">Find out more</a></p>
 </div>
 </li>
+<?php dpm($row); ?>
