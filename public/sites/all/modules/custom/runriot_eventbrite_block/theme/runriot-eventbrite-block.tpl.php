ab046c28e17be28762dd7e5b60e1921b802c25b6
Andy Brace <contact@openbook.uk.com>
2014-08-17 20:59:15 +0100

    updating redirect for mobile

--- public/sites/all/modules/custom/runriot_eventbrite_block/theme/runriot-eventbrite-block.tpl.php
+++ public/sites/all/modules/custom/runriot_eventbrite_block/theme/runriot-eventbrite-block.tpl.php
@@ -41,7 +41,7 @@ $concession_state = FALSE;
   <li class="sold-out">
     <h3>Full price</h3>
     <p>&pound;<?php print money_format($tickets['full']->display_price, 2);?> <span>Sold out</span></p>
-    <a href="/rr/mailinglist/nojs/<?php print $nid; ?>" class="btn btn-alt ctools-use-modal">Join waiting list</a>
+    <a href="/rr/mailinglist/nojs/<?php print $nid; ?>" class="btn btn-alt ctools-use-modal waitinglist-signup">Join waiting list</a>
   </li>
   <?php endif; ?>
 
@@ -55,7 +55,7 @@ $concession_state = FALSE;
   <li class="sold-out">
     <h3>Concessions <span class="tooltip"></span></h3>
     <p>&pound;<?php print money_format($tickets['concession']->display_price, 2);?> <span>Sold out</span></p>
-    <a href="/rr/mailinglist/nojs/<?php print $nid; ?>" class="btn btn-alt ctools-use-modal">Join waiting list</a>
+    <a href="/rr/mailinglist/nojs/<?php print $nid; ?>" class="btn btn-alt ctools-use-modal waitinglist-signup">Join waiting list</a>
   </li>
   <?php endif; ?>
 </ul>
--- public/sites/all/themes/custom/runriot_zen/js/mobile.js
+++ public/sites/all/themes/custom/runriot_zen/js/mobile.js
@@ -1,5 +1,12 @@
 $( document ).ready(function() {
   if (screen.width <= 980) {
     $("a.waitinglist-signup").prop('href', '/signup/mailinglist');
+// $("a.ctools-use-modal").removeClass("ctools-use-modal");
+// $("a.ctools-use-modal").unbind();
+// $("a.waitinglist-signup").bind("click", function() {
+// alert("done2");
+// window.location.href = "http://stackoverflow.com";
+// });
+// alert("done1");
   }
 });
